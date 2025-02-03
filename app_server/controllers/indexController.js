d1ac4c2d112848b21fbe97a800eb7a61620b2033
Ahnaf Zaman <ahnafzaman@mac.myfiosgateway.com>
2025-02-02 21:22:33 -0500

    Completed Module 3 task

--- app_server/controllers/indexController.js
+++ app_server/controllers/indexController.js
@@ -1,4 +1,20 @@
+// module.exports.homePage = (req, res) => {
+//     res.render('index', { title: 'Travlr Getaways Website Template' });
+//   };
+
+const fs = require('fs');
+const path = require('path');
+
 module.exports.homePage = (req, res) => {
-    res.render('index', { title: 'Travlr Getaways Website Template' });
-  };
+    const tripsFilePath = path.join(__dirname, '../../trips.json');
+
+    
+    const tripsData = JSON.parse(fs.readFileSync(tripsFilePath, 'utf-8'));
+    
+    res.render('index', { 
+        title: 'Travlr Getaways Website Template',
+        trips: tripsData 
+    });
+};
+
   
